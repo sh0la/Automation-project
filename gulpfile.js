6098cc3833ec28765645e490aefbb46c95f48a57
sh0la <sholaadeniyi@gmail.com>
2019-12-06 03:47:05 -0600

    Create gulp watching functionality

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const {src, dest, series} = require('gulp');
+const {src, dest, series, watch} = require('gulp');
 const autoprefix = require('gulp-autoprefixer');
 const cleancss = require('gulp-clean-css');
 const babel = require('gulp-babel');
@@ -6,40 +6,42 @@ const concatenate = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
 
-function html(cb) {
-  src('./src/index.html')
+function html() {
+  return src('./src/index.html')
     .pipe(dest('./dest'))
-    cb()
+    
 }
 
-function styles(cb) {
-  src('./src/css/**/*.css')
-    .pipe(autoprefix())
+function styles() {
+  return src('./src/css/**/*.css')
+    .pipe(autoprefix('last 2 versions'))
     .pipe(cleancss())
     .pipe(dest('./dest/css'))
-    cb()
 }
 
-function reimage(cb) {
-  src('./src/images/**/*')
-  .pipe(imagemin())
-  .pipe(dest('./dest/images'))
+function reimage() {
+  return src('./src/images/**/*')
+    .pipe(imagemin())
+    .pipe(dest('./dest/images'))
+}
+
+
+function js() {
+  return src([
+      './src/js/**/resources.js',
+      './src/js/**/app.js',
+      './src/js/**/engine.js'
+    ])
+      .pipe(concatenate('main.js'))
+      .pipe(dest('./dest/js'))
+}
+
+function watching(cb) {
+  watch('./src/index.html').on('change', html)
+  watch('./src/js/**/*.js').on('change', js)
+  watch('./src/css/**/*.css').on('change', styles)
   cb()
 }
 
-
-function js(cb) {
-  //src('./src/js/**/*.js')
-
-  src([
-    './src/js/**/resources.js',
-    './src/js/**/app.js',
-    './src/js/**/engine.js'
-  ])
-    .pipe(concatenate('main.js'))
-    .pipe(dest('./dest/js'))
-    cb()
-}
-
 //exports.js = js;
-exports.default = series(html, styles, js)
+exports.default = series(html, styles, js, reimage, watching)
